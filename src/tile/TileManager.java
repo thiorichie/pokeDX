a7bdf38a03ac818f37e4eecb2ec3c67894eff1f2
William2152 <133088365+William2152@users.noreply.github.com>
2023-06-06 12:19:54 +0700

    map

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -31,6 +31,11 @@ public class TileManager {
         loadMap("/maps/Village.txt",1);
         loadMap("/maps/Rumah1.txt",2);
         loadMap("/maps/Rumah2.txt",3);
+<<<<<<< Updated upstream
+=======
+        loadMap("/maps/protomap002.txt",1);
+        loadMap("/maps/Village.txt", 2);
+>>>>>>> Stashed changes
     }
     
     public void getTileImage(){
@@ -127,6 +132,11 @@ public class TileManager {
             tile[24] = new Tile();
             tile[24].image = ImageIO.read(getClass().getResourceAsStream("/rumah1/9.png"));
             tile[24].collision = true;
+<<<<<<< Updated upstream
+=======
+
+            
+>>>>>>> Stashed changes
         } 
         catch (IOException e) {
             e.printStackTrace();
